d307341ec9db4e5f48a0b34d946db1cf9a1c061e
Artur Carvalho <arturpedroso5050@gmail.com>
2024-11-21 07:33:33 -0300

    implementa fluxo api

--- src/main/java/br/com/petz/cliente_pet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetAPI.java
@@ -1,11 +1,13 @@
 package br.com.petz.cliente_pet.pet.application.api;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteListResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -15,4 +17,8 @@ public interface PetAPI {
     @ResponseStatus(code = HttpStatus.CREATED)
     PetResponse postPet(@PathVariable UUID idCliente,
             @Valid @RequestBody PetRequest petRequest);
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    List<petClienteListResponse> getPetsDoClienteComId(@PathVariable UUID idCliente);
 }
